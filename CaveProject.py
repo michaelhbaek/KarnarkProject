7c024fcd44860a743c2239afa92bb8d2ff056694
hansol13 <michaelbaek96@gmail.com>
2018-04-11 16:59:11 -0400

    first commit
    first commit

---
+++ CaveProject.py
@@ -0,0 +1,15 @@
+﻿# CAVE project
+
+import viz
+import vizshape
+vizshape.addAxes()
+
+viz.go()
+
+karnark = viz.addChild('KarnakFBX.fbx')
+
+viz.mouse.setScale( 100, 2) # 100x movement speed, 2x rotation speed
+viz.move([10,10,10]) # i think this sets the starting position to 10,10,10
+plant = viz.addChild('plant.osgb') # just wanted to see if we can see this - i cant but maybe you guys can?
+plant.setPosition([4,0,4])
+viz.collision(viz.ON) #user will collide with walls and stop moving
