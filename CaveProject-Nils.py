d46c66c1b6b50331beb2888f9c167ae51f58a2ab
Nils Lofgren <nlofgren@villanova.edu>
2018-04-16 01:32:58 -0400

    Fixed proximity sensors
    Working code to change infopanel's text on entrance to each site
    Press d to show collision boxes so you know where to go.

--- CaveProject-Nils.py
+++ CaveProject-Nils.py
@@ -1,4 +1,4 @@
-﻿# CAVE projectM
+# CAVE projectM
 
 import viz
 import vizshape
@@ -9,35 +9,22 @@ import vizproximity
 env = viz.addEnvironmentMap('sky.jpg')
 viz.clearcolor(viz.SKYBLUE)
 
-viz.mouse.setScale( 1000, 2) # 100x movement speed, 2x rotation speed
+viz.mouse.setScale( 100, 2) # 100x movement speed, 2x rotation speed
 viz.move([10,10,10]) # i think this sets the starting position to 10,10,10
 
 karnak = viz.addChild('KarnakFBX.fbx')
 viz.setMultiSample(4)
 viz.go()
 vizshape.addAxes()
-info = vizinfo.InfoPanel(icon=False,key=None)
+info = vizinfo.InfoPanel("Let get started")
 
 
 
 
-
-stores = viz.addChild()
-stores.setPosition([3032.49878, 462.99207, -7990.59961])
-
-ramesses = viz.addChild()
-ramesses.setPosition([11496.70117, 578.93695, -2284.39844])
-
-taharqa = viz.addChild()
-taharqa.setPosition([4090.52271, 375.59061, -1435.92212])
-
-amun = viz.addChild()
-amun.setPosition([6657.56494, 504.16730, -1794.23059])
-
-sensorAvatar1 = vizproximity.Sensor(vizproximity.Box([200,250,250],center=[3032.49878, 462.99207, -7990.59961]))
-sensorAvatar2 = vizproximity.Sensor(vizproximity.Box([200,250,250],center=[11496.70117, 578.93695, -2284.39844]))
-sensorAvatar3 = vizproximity.Sensor(vizproximity.Box([200,250,250],center=[4090.52271, 375.59061, -1435.92212]))
-sensorAvatar4 = vizproximity.Sensor(vizproximity.Box([200,250,250],center=[6657.56494, 504.16730, -1794.23059]))
+sensorAvatar1 = vizproximity.Sensor(vizproximity.Box([150,200,200],center=[3032.49878, 462.99207, -7990.59961]), source=karnak)
+sensorAvatar2 = vizproximity.Sensor(vizproximity.Box([150,200,200],center=[11496.70117, 578.93695, -2284.39844]), source=karnak)
+sensorAvatar3 = vizproximity.Sensor(vizproximity.Box([150,200,200],center=[4090.52271, 375.59061, -1435.92212]), source=karnak)
+sensorAvatar4 = vizproximity.Sensor(vizproximity.Box([150,200,200],center=[6657.56494, 504.16730, -1794.23059]), source=karnak)
 
 target = vizproximity.Target(viz.MainView)
 
@@ -56,12 +43,16 @@ def EnterProximity(e):
 	"""@args vizproximity.ProximityEvent()"""
 	
 	if e.sensor == sensorAvatar1:
+		print "We made it to the stores!"
 		info.setText('Stores')
 	elif e.sensor == sensorAvatar2:
+		print "We made it to Ramesses"
 		info.setText('ramesses')
 	elif e.sensor == sensorAvatar3:
+		print "We made it to Taharqa!"
 		info.setText('taharqa')
 	elif e.sensor == sensorAvatar4:
+		print "we made it to Amun!"
 		info.setText('amun')
 		
 manager.onEnter(None,EnterProximity)
